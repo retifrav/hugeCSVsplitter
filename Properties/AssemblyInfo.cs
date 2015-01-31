6e7a6a4ff3b3ea4f0d8a74edd94be909b92c9973
retif <retifrav@gmail.com>
2015-01-31 23:00:56 +0300

    год и автор

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("hugeCSVsplitter")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("retif, 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
